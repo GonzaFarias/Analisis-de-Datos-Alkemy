cd4187f9f7b29f79aa05a1bf48e6576f439d9442
Gonzalo <gonzaxeneize01@gmail.com>
2024-04-27 20:47:34 -0300

    add path

---
+++ Proyecto Final/config.py
@@ -0,0 +1,26 @@
+class Config:
+    BASE_PATH = 'Proyecto Final/data/'
+
+    @staticmethod
+    def get_dataset_path(filename):
+        return Config.BASE_PATH + filename
+
+    @classmethod
+    def customers_path(cls):
+        return cls.get_dataset_path('ecommerce_customers_dataset.csv')
+
+    @classmethod
+    def items_path(cls):
+        return cls.get_dataset_path('ecommerce_order_items_dataset.csv')
+
+    @classmethod
+    def payments_path(cls):
+        return cls.get_dataset_path('ecommerce_order_payments_dataset.csv')
+
+    @classmethod
+    def orders_path(cls):
+        return cls.get_dataset_path('ecommerce_orders_dataset.csv')
+
+    @classmethod
+    def products_path(cls):
+        return cls.get_dataset_path('ecommerce_products_dataset.csv')
