4b838b0c5fcd6f75ed41ff2cd9787b753c6869b3
Gonzalo <gonzaxeneize01@gmail.com>
2024-04-27 22:45:16 -0300

    strategy implement

--- Proyecto Final/config.py
+++ Proyecto Final/config.py
@@ -6,21 +6,22 @@ class Config:
         return Config.BASE_PATH + filename
 
     @classmethod
-    def customers_path(cls):
+    def customers_path(cls) -> str:
         return cls.get_dataset_path('ecommerce_customers_dataset.csv')
 
     @classmethod
-    def items_path(cls):
+    def items_path(cls) -> str:
         return cls.get_dataset_path('ecommerce_order_items_dataset.csv')
 
     @classmethod
-    def payments_path(cls):
+    def payments_path(cls) -> str:
         return cls.get_dataset_path('ecommerce_order_payments_dataset.csv')
 
     @classmethod
-    def orders_path(cls):
+    def orders_path(cls) -> str:
         return cls.get_dataset_path('ecommerce_orders_dataset.csv')
 
     @classmethod
-    def products_path(cls):
+    def products_path(cls) -> str:
         return cls.get_dataset_path('ecommerce_products_dataset.csv')
+
--- Proyecto Final/tp_final.py
+++ Proyecto Final/tp_final.py
@@ -1,14 +1,54 @@
 import pandas as pd
+import numpy as np
+from config import Config
+from abc import ABC, abstractmethod
 
-def read_file(path: str) -> pd.DataFrame:
-    extension = path.split(".")[-1] # archivo.csv = ["archivo", "csv"]
-    if extension == "csv":
+# Estrategia para la lectura de datasets
+class FileReader(ABC):
+    @abstractmethod
+    def read(self, path: str) -> pd.DataFrame:
+        pass
+
+class CSVFileReader(FileReader):
+    def read(self, path: str) -> pd.DataFrame:
         return pd.read_csv(path)
-    elif extension in ["xls", "xlsx"]:
-        return pd.read_excel(path)
-    elif extension == "json":
-        return pd.read_json(path)
-    else: 
-        raise ValueError("Extension no soportada")
 
- 
+class ExcelFileReader(FileReader):
+    def read(self, path: str) -> pd.DataFrame:
+        return pd.read_excel(path)
+
+class JSONFileReader(FileReader):
+    def read(self, path: str) -> pd.DataFrame:
+        return pd.read_json(path)
+
+class FileReaderContext:
+    def __init__(self, reader: FileReader):
+        self.reader = reader
+
+    def read_file(self, path: str) -> pd.DataFrame:
+        return self.reader.read(path)
+
+# Lectura de datasets
+def read_file(path: str) -> pd.DataFrame:
+    extension = path.split(".")[-1]
+    if extension == "csv":
+        reader = CSVFileReader()
+    elif extension in ["xls", "xlsx"]:
+        reader = ExcelFileReader()
+    elif extension == "json":
+        reader = JSONFileReader()
+    else:
+        raise ValueError("Extension no soportada")
+    context = FileReaderContext(reader)
+    return context.read_file(path)
+
+# Creación de datasets
+df_customers = read_file(Config.customers_path())
+df_items = read_file(Config.items_path())
+df_payments = read_file(Config.payments_path())
+df_orders = read_file(Config.orders_path())
+df_products = read_file(Config.products_path())
+
+# Información de los datos
+print(df_orders.describe())
+print(df_payments)
