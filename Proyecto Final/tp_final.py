c176b86859c49c3b31cbce96e1b83a4a5d773f35
Gonzalo <gonzaxeneize01@gmail.com>
2024-04-28 22:33:12 -0300

    clean datasets

--- Proyecto Final/tp_final.py
+++ Proyecto Final/tp_final.py
@@ -49,6 +49,33 @@ df_payments = read_file(Config.payments_path())
 df_orders = read_file(Config.orders_path())
 df_products = read_file(Config.products_path())
 
-# Información de los datos
-print(df_orders.describe())
-print(df_payments)
+# Limpieza de datos
+def clean_dataset(df:pd.DataFrame) -> pd.DataFrame:
+    # Eliminar registros duplicados
+    df.drop_duplicates()
+
+    # Eliminar filas con valores nulos en columnas de tipo string
+    df.dropna(subset=df.select_dtypes(include=['object']).columns, inplace=True)
+
+    # Rellenar los valores nulos con la media en columnas numéricas
+    numeric_columns = df.select_dtypes(include=['number']).columns
+    df[numeric_columns] = df[numeric_columns].fillna(df[numeric_columns].mean())
+
+    # Eliminar filas completas si contienen valores nulos
+    df.dropna(inplace=True)
+    return df
+
+# Aplicar la limpieza a todos los datasets
+df_customers_copy = clean_dataset(df_customers)
+df_items_copy = clean_dataset(df_items)
+df_payments_copy = clean_dataset(df_payments)
+df_orders_copy = clean_dataset(df_orders)
+df_products_copy = clean_dataset(df_products)
+
+# Obtener descripciones de cada conjunto de datos limpios
+print(df_customers_copy.describe())
+print(df_items_copy.describe())
+print(df_payments_copy.describe())
+print(df_orders_copy.describe())
+print(df_products_copy.describe())
+
