bd83afb8c6f4e05380800a8a18c818d5632c4367
Gonzalo <gonzaxeneize01@gmail.com>
2024-04-29 17:21:35 -0300

    add plots

--- Proyecto Final/tp_final.py
+++ Proyecto Final/tp_final.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from config import Config
 from abc import ABC, abstractmethod
+import gc
 
 # Estrategia para la lectura de datasets
 class FileReader(ABC):
@@ -55,22 +56,19 @@ def clean_dataset(df:pd.DataFrame) -> pd.DataFrame:
     # Eliminar registros duplicados
     df.drop_duplicates()
 
-    # Eliminar filas con valores nulos en columnas de tipo string
-    df.dropna(subset=df.select_dtypes(include=['object']).columns, inplace=True)
-
     # Rellenar los valores nulos con la media en columnas numéricas
     numeric_columns = df.select_dtypes(include=['number']).columns
     df[numeric_columns] = df[numeric_columns].fillna(df[numeric_columns].mean())
 
-    # Eliminar filas completas si contienen valores nulos
-    df.dropna(inplace=True)
+    # Eliminar filas completas si todos son valores nulos
+    df.dropna(how='all',inplace=True)
     return df
 
-# Aplicar la limpieza a todos los datasets
+# Aplicar la limpieza a todos los datasets y ordenamiento
 df_customers_copy = clean_dataset(df_customers)
 df_items_copy = clean_dataset(df_items)
 df_payments_copy = clean_dataset(df_payments)
-df_orders_copy = clean_dataset(df_orders)
+df_orders_copy = clean_dataset(df_orders).sort_values(by='order_purchase_timestamp', ascending=False)
 df_products_copy = clean_dataset(df_products)
 
 # Obtener información de cada conjunto de datos 
@@ -92,8 +90,32 @@ plt.ylabel('Valor')
 plt.title('Boxplot de precio')
 plt.show()
 
-# Al ver el boxplot de los precios, podemos identificar la gran variacion entre estos
+# Al ver el boxplot de los precios, podemos identificar la variacion entre estos
 # mostrando gran distancia entre el maximo y minimo valor.
 print('\nDatos estadisticos del precio de productos\n',df_items_copy['price'].describe(),'\n')
 
+gc.collect()
+
+# Cantidad de clientes por estado
+df_customers_copy['customer_state'].hist()
+plt.xlabel('Estado')
+plt.ylabel('Clientes')
+plt.title('Ciudades por cliente')
+plt.show()
+
+# Frecuencia de tipos de pago
+df_payments_copy['payment_type'].hist()
+plt.xlabel('Tipo de pago')
+plt.ylabel('Cantidad')
+plt.title('Frecuencia tipos de pago')
+plt.show()
+
+# Gráfico de dispersión entre precio del producto y valor del flete
+plt.scatter(df_items_copy['price'], df_items_copy['freight_value'])
+plt.xlabel('Precio')
+plt.ylabel('Valor del flete')
+plt.title('Gráfico de dispersión')
+plt.show()
+
+df_orders_copy.to_excel('Proyecto Final\data\Orders.xlsx', index=False)
 
