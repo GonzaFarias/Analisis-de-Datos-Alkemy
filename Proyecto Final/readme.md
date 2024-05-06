79b5ebfcb8c2a0b4a18edb6b45966e3e4753585a
Gonzalo <gonzaxeneize01@gmail.com>
2024-05-06 00:21:35 -0300

    add plots and docstring + export data

---
+++ Proyecto Final/readme.md
@@ -0,0 +1,66 @@
+## Trabajo Práctico Integrador - Análisis de Datos
+
+###### Autor: Gonzalo Farías
+
+###### Fecha: 07/05/2024
+
+---
+
+### Resumen
+
+Este trabajo presenta un análisis de datos realizado sobre un conjunto de datos del sector de comercio. Se aplicaron diversas técnicas de limpieza, exploración y visualización de datos para comprender mejor la información contenida en los conjuntos de datos proporcionados.
+
+---
+
+### Conclusiones
+
+1. **Cantidad de Clientes Únicos:**
+
+   - Se identificó un total de 96096 clientes únicos en el conjunto de datos de clientes.
+2. **Promedio del Valor de Pago por Pedido:**
+
+   - El promedio del valor de pago por pedido se situó en $154.1.
+3. **Categoría de Productos Más Vendida:**
+
+   - La categoría de productos más vendida fue "cama_mesa_baño", con un valor aproximado de 11500 ventas.
+     [Plot ventas por categoría](plots\plot1.png "Imagen")
+4. **Número Total de Pedidos Realizados:**
+
+   - Se registraron un total de 98816 pedidos completados, excluyendo aquellos que fueron cancelados.
+5. **Estadísticas de Precios de Productos:**
+
+   - El análisis estadístico de los precios de los productos reveló una variación significativa entre los precios, con un rango que va desde 0.85 hasta 6735
+     [Boxplot de precios](plots\plot2.png "Imagen")
+6. **Distribución de Clientes por Estado:**
+
+   - La mayoría de los clientes se encuentran en los estados de "SP" y "SC", seguidos por "MG".
+     [Clientes por estado](plots\plot3.png "imagen")
+7. **Frecuencia de Tipos de Pago:**
+
+   - El tipo de pago más frecuente fue "credit_card", seguido por "boleto" y "voucher".
+     [Frecuencia tipos de pago](plots\plot4.png "imagen")
+8. **Relación entre Precio del Producto y Valor del Flete:**
+
+   - No se observó una relación clara entre el precio del producto y el valor del flete, según el gráfico de dispersión generado.
+     [Gráfico de dispersion](plots\plot5.png "imagen")
+9. **Cantidad de Compras por Mes:**
+
+   - Se observó una tendencia de crecimiento en la cantidad de ventas a lo largo del tiempo, llegando a los maximos valores a partir del año 2018 en octubre.
+     [Compras por mes](plots\plot6.png "imagen")
+
+---
+
+### Recomendaciones
+
+1. **Enfoque en la Categoría de Productos Más Vendida:**
+
+   - Dado que la categoría de productos "cama_mesa_baño" es la más vendida, se recomienda enfocar las estrategias de marketing y ventas en esta categoría para maximizar los ingresos.
+2. **Expansión en Regiones con Mayor Concentración de Clientes:**
+
+   - Considerando la distribución de clientes por estado, se sugiere enfocar los esfuerzos de expansión en regiones como "SP" y "SC", donde se concentra la mayor parte de la clientela.
+3. **Monitoreo Continuo de Tendencias de Ventas:**
+
+   - Dado el análisis de la cantidad de compras por mes, se recomienda realizar un monitoreo continuo de las tendencias de ventas para identificar patrones estacionales o cambios en el comportamiento del consumidor.
+4. **Mejora de la Logística de Envíos:**
+
+   - Dado que no se observó una relación clara entre el precio del producto y el valor del flete, se sugiere mejorar la logística de envíos para optimizar los costos y tiempos de entrega.
--- Proyecto Final/tp_final.py
+++ Proyecto Final/tp_final.py
@@ -1,3 +1,8 @@
+# Trabajo Práctico Integrador - Análisis de Datos
+# Gonzalo Farías
+# 07/05/2024
+
+# Importación de recursos necesarios
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,22 +14,27 @@ import gc
 class FileReader(ABC):
     @abstractmethod
     def read(self, path: str) -> pd.DataFrame:
+        """Lee un archivo y devuelve un DataFrame."""
         pass
 
 class CSVFileReader(FileReader): # Lector csv
     def read(self, path: str) -> pd.DataFrame:
+        """Lee un archivo CSV y devuelve un DataFrame."""
         return pd.read_csv(path)
 
 class ExcelFileReader(FileReader): # Lector excel
     def read(self, path: str) -> pd.DataFrame:
+        """Lee un archivo Excel y devuelve un DataFrame."""
         return pd.read_excel(path)
 
 class JSONFileReader(FileReader): # Lector json
     def read(self, path: str) -> pd.DataFrame:
+        """Lee un archivo JSON y devuelve un DataFrame."""
         return pd.read_json(path)
 
 class TXTFileReader(FileReader): # Lector txt
-    def read(self, path: str):
+    def read(self, path: str) -> np.ndarray:
+        """Lee un archivo de texto y devuelve un array de NumPy."""
         return np.loadtxt(path)      
 
 class FileReaderContext: 
@@ -32,11 +42,23 @@ class FileReaderContext:
         self.reader = reader
 
     def read_file(self, path: str) -> pd.DataFrame:
+        """Utiliza el FileReader proporcionado para leer un archivo y devolver un DataFrame."""
         return self.reader.read(path)
 
 # Lectura de datasets
-def read_file(path: str) -> pd.DataFrame: # Devuelve un dataframe según el tipo de archivo leido
-    extension = path.split(".")[-1]
+def read_file(path: str) -> pd.DataFrame:
+    """Devuelve un DataFrame según el tipo de archivo leído.
+    
+    Args:
+        path (str): La ruta al archivo que se va a leer.
+
+    Raises:
+        ValueError: Si la extensión del archivo no es soportada.
+
+    Returns:
+        pd.DataFrame: El DataFrame resultante de leer el archivo.
+    """
+    extension = path.split(".")[-1].lower()
     if extension == "csv":
         reader = CSVFileReader()
     elif extension in ["xls", "xlsx"]:
@@ -46,9 +68,60 @@ def read_file(path: str) -> pd.DataFrame: # Devuelve un dataframe según el tipo
     elif extension == 'txt':
         reader = TXTFileReader()    
     else:
-        raise ValueError("Extension no soportada")
+        raise ValueError("Extensión no soportada")
     context = FileReaderContext(reader)
     return context.read_file(path)
+
+# Limpieza de datos
+def clean_dataset(df: pd.DataFrame) -> pd.DataFrame:
+    """Limpia el DataFrame proporcionado eliminando duplicados y valores nulos.
+
+    Args:
+        df (pd.DataFrame): El DataFrame a limpiar.
+
+    Returns:
+        pd.DataFrame: El DataFrame limpio.
+    """
+    df.drop_duplicates()
+    # numeric_columns = df.select_dtypes(include=['number']).columns 
+    # df[numeric_columns] = df[numeric_columns].fillna(df[numeric_columns].mean()) """ # Decidi no utilizarlo al no ser aplicable en este caso
+    df.dropna(how='all')
+    return df
+
+# Obtener información de cada conjunto de datos  
+def dataset_info(df: pd.DataFrame):
+    """Imprime información descriptiva del DataFrame proporcionado.
+
+    Args:
+        df (pd.DataFrame): El DataFrame del cual se obtendrá la información.
+    """
+    print(4 * '-------------------------------' + '\nDatos descriptivos:')
+    print(f'{df.info()}\n')
+    print(f'{df.describe()}\n')
+
+# Adicional - Uso de NumPy para cálculos de álgebra lineal
+def calculos_numpy():
+    """Realiza y muestra varios cálculos de álgebra lineal utilizando NumPy."""
+    x = np.array([1,2])
+    y = np.array([3,4])
+    sum_list = x + y
+    print(f'Suma de listas {x} e {y} con numpy: ',sum_list)
+
+    # Matriz identidad:
+    print(f'\nCreación de matriz identidad:\n{np.eye(3)}\n')
+
+    # Producto punto:
+    print(f'El resultado del producto punto entre {x} e {y} es:{np.dot(x, y)}')
+    
+    # Calcular determinante:
+    matrix = np.array([[1, 2], [3, 4]])
+    print(f'\nEl determinante de la matriz {matrix} es:\n{np.linalg.det(matrix)}')
+
+    # Matriz de ceros:
+    print ('\nMatriz de ceros: \n',np.zeros((4,4)))
+
+    # Producto cruzado:
+    print(f'\nEl resultado del producto cruzado entre {x} e {y} es: {np.cross(x, y)}' )
 
 # Creación de datasets
 df_customers = read_file(Config.customers_path())
@@ -57,39 +130,21 @@ df_payments = read_file(Config.payments_path())
 df_orders = read_file(Config.orders_path())
 df_products = read_file(Config.products_path())
 
-# Limpieza de datos
-def clean_dataset(df:pd.DataFrame) -> pd.DataFrame:
-    # Eliminar registros duplicados
-    df.drop_duplicates()
-
-    # Rellenar los valores nulos con la media en columnas numéricas
-    numeric_columns = df.select_dtypes(include=['number']).columns
-    df[numeric_columns] = df[numeric_columns].fillna(df[numeric_columns].mean())
-
-    # Eliminar filas completas si todos son valores nulos
-    df.dropna(how='all',inplace=True)
-    return df
-
 # Aplicar la limpieza a todos los datasets y ordenamiento
 df_customers_copy = clean_dataset(df_customers)
 df_items_copy = clean_dataset(df_items)
 df_payments_copy = clean_dataset(df_payments)
-df_orders_copy = clean_dataset(df_orders).sort_values(by='order_approved_at', ascending=False) # Ordenado por fecha de aprobación de la compra
+df_orders_copy = clean_dataset(df_orders).sort_values(by='order_approved_at', ascending=False).iloc[1:] # Ordenado por fecha de aprobación de la compra
 df_products_copy = clean_dataset(df_products)
 
 # Asignación de indices a los datasets
-df_customers_copy.set_index('customer_id', inplace=True, drop=False)
-df_items_copy.set_index(['order_id','order_item_id'], inplace=True, drop=False) 
-df_payments_copy.set_index(['order_id','payment_sequential'], inplace=True, drop=False) 
-df_orders_copy.set_index(['order_id','customer_id'], inplace=True, drop=False) 
-df_products_copy.set_index('product_id', inplace=True, drop=False) 
+df_customers_copy.set_index('customer_id', inplace=True)
+df_items_copy.set_index(['order_id','product_id'], inplace=True) 
+df_payments_copy.set_index(['order_id','payment_sequential'], inplace=True) 
+df_orders_copy.set_index('order_id', inplace=True) 
+df_products_copy.set_index('product_id', inplace=True) 
 
-# Obtener información de cada conjunto de datos  
-def dataset_info(df: pd.DataFrame):
-    print(4 * '-------------------------------' + '\nDatos descriptivos:')
-    print(f'{df.info()}\n')
-    print(f'{df.describe()}\n')
-
+# Ver información de los datasets
 dataset_info(df_customers_copy)
 dataset_info(df_items_copy)
 dataset_info(df_payments_copy)
@@ -97,16 +152,45 @@ dataset_info(df_orders_copy)
 dataset_info(df_products_copy)
 
 # Cantidad total de clientes únicos en el conjunto de datos
-print("\nCantidad de clientes únicos:", df_customers_copy['customer_unique_id'].nunique())
+unique_customers = df_customers_copy['customer_unique_id'].nunique()
+print("\nCantidad de clientes únicos:", unique_customers)
+
+# Cantidad promedio del valor de pago por pedido
+average_payment_value = df_payments_copy['payment_value'].mean()
+print("\nPromedio del valor de pago por pedido:", average_payment_value)
+
+# Merge de df_order_items y df_products para poder calcular la cantidad de ventas por categoría
+merged_df_order_products = pd.merge(df_items_copy, df_products_copy, on='product_id', how='inner')
+
+# Agrupación por categoria de producto
+grouped_df_order_products = merged_df_order_products.groupby('product_category_name').size().reset_index(name='count')
+
+# Visualización de la venta por categorías de producto, para identificar
+# cual es la más vendida.
+plt.bar(grouped_df_order_products['product_category_name'], grouped_df_order_products['count'])
+plt.xlabel('Categoría')
+plt.ylabel('Ventas')
+plt.title('Ventas por categoría de producto')
+plt.xticks(rotation=90)  
+plt.savefig("Proyecto Final\plots\plot1.png")
+plt.show() # Podemos observar como la categoría de productos más vendida es cama_mesa_banho,
+# que serian los productos relacionados con cama, mesa y baños.
+
+# Cálculo de la categoría de productos más vendida
+max_selling_category = grouped_df_order_products.sort_values(by='count', ascending=False).iloc[0]['product_category_name']
+print('Categoría de productos más vendida:',max_selling_category)
+
+# Cálculo del número total de pedidos completados, o sea, excluyendo los cancelados
+total_orders = len(df_orders_copy[df_orders_copy['order_status'] != 'canceled'])
+print(f'Número total de pedidos realizados: {total_orders}')
 
 # Visualizar boxplot de precios
 plt.boxplot(df_items_copy['price'])
 plt.xlabel('Precio')
 plt.ylabel('Valor')
 plt.title('Boxplot de precio')
-plt.show()
-
-# Al ver el boxplot de los precios, podemos identificar la variacion entre estos
+plt.savefig("Proyecto Final\plots\plot2.png")
+plt.show() # Al ver el boxplot de los precios, podemos identificar la variacion entre estos
 # mostrando gran distancia entre el maximo y minimo valor.
 print('\nDatos estadisticos del precio de productos\n',df_items_copy['price'].describe(),'\n')
 
@@ -116,7 +200,8 @@ gc.collect()
 df_customers_copy['customer_state'].hist()
 plt.xlabel('Estado')
 plt.ylabel('Clientes')
-plt.title('Ciudades por cliente')
+plt.title('Clientes por estado')
+plt.savefig("Proyecto Final\plots\plot3.png")
 plt.show()
 
 # Visualización frecuencia de tipos de pago
@@ -124,6 +209,7 @@ df_payments_copy['payment_type'].hist()
 plt.xlabel('Tipo de pago')
 plt.ylabel('Cantidad')
 plt.title('Frecuencia tipos de pago')
+plt.savefig("Proyecto Final\plots\plot4.png")
 plt.show()
 
 # Gráfico de dispersión entre precio del producto y valor del flete
@@ -131,6 +217,7 @@ plt.scatter(df_items_copy['price'], df_items_copy['freight_value'])
 plt.xlabel('Precio')
 plt.ylabel('Valor del flete')
 plt.title('Gráfico de dispersión')
+plt.savefig("Proyecto Final\plots\plot5.png")
 plt.show() # Se observa que el valor del flete no está relacionado con el costo del producto
 
 # Visualización cantidad de compras por mes
@@ -144,7 +231,18 @@ plt.title('Cantidad de Ventas por Mes')
 plt.xticks(rotation=45)  
 plt.tight_layout() 
 plt.grid(True)  
+plt.savefig("Proyecto Final\plots\plot6.png")
 plt.show()
 
 # Exportación de datos limpios
-df_orders_copy.to_excel('Proyecto Final\data\datos_limpios.xlsx', index=True)
+with pd.ExcelWriter('Proyecto Final\data\clean_data.xlsx') as writer:
+    df_customers_copy.to_excel(writer, sheet_name='ecommerce_customers_clean', index=True)
+    df_items_copy.to_excel(writer, sheet_name='ecommerce_items_clean', index=True)
+    df_payments_copy.to_excel(writer, sheet_name='ecommerce_payments_clean', index=True)
+    df_orders_copy[df_orders_copy['order_status'] != 'canceled'].to_excel(writer, sheet_name='ecommerce_orders_clean', index=True)
+    df_products_copy.to_excel(writer, sheet_name='ecommerce_products_clean', index=True)
+    
+# Vemos algunos de los calculos que se puedenr realizar con NumPy
+calculos_numpy()
+
+   
